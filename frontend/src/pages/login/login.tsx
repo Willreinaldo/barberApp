01acc7aafb251cb4dd029a058e32fd4bd54d7a2a
franciscojrdev <franciscojunior7222@gmail.com>
2024-08-03 00:42:05 -0300

    add logo

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import logo from "./logo.png";
 
 const ModalBackground = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  position: relative;
   align-items: center;
   height: 100vh;
-  background-color: #0051B1;
+  background-color: #0051b1;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  margin-bottom: 20px;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+`;
+
+const TitleLogo = styled.h1`
+  font-size: 40px;
+  margin: 0;
 `;
 
 const Modal = styled.div`
@@ -17,24 +36,29 @@ const Modal = styled.div`
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   max-width: 400px;
-  /* width: 100%; */
+  width: 100%;
   text-align: center;
+`;
+
+const Title = styled.h1`
   color: #fff;
+  font-size: 36px;
 `;
 
 const Input = styled.input`
-  width: 100%;
+  width: 90%;
   padding: 10px;
   margin: 10px 0;
   border: 1px solid #ccc;
   border-radius: 4px;
+  outline: none;
 `;
 
 const Button = styled.button`
   padding: 10px 20px;
   border: none;
   border-radius: 4px;
-  background-color: #007BFF;
+  background-color: #007bff;
   color: white;
   cursor: pointer;
   &:hover {
@@ -43,19 +67,23 @@ const Button = styled.button`
 `;
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
     // Lógica de login aqui
-    console.log('Email:', email);
-    console.log('Password:', password);
+    console.log("Email:", email);
+    console.log("Password:", password);
   };
 
   return (
     <ModalBackground>
+      <Container>
+        <Logo src={logo} alt="Logo" />
+        <Title>BARBER SHOP</Title>
+      </Container>
       <Modal>
-        <h2>Login</h2>
+        <Title>Login</Title>
         <Input
           type="email"
           placeholder="Email"
