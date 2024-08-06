9af980753b6b17dde0befcdbdcd5593684f16554
franciscojrdev <franciscojunior7222@gmail.com>
2024-08-05 22:43:45 -0300

    feat: prisma migrate and config postgres database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
-import express from 'express';
-import cors from 'cors';
+import express, { json, Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(json());
 app.use(cors());
-app.use(express.json());
 
-app.use('/api');
+app.get("/health", (req: Request, res: Response) => res.send("Hello world"));
+
+// app.use("/api");
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
