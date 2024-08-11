d1e6a1643406330e07a6509a506522c46e3ebef7
franciscojrdev <franciscojunior7222@gmail.com>
2024-08-10 23:23:14 -0300

    back config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
-import express, { json, Request, Response } from "express";
-import cors from "cors";
+import { PrismaClient } from '@prisma/client'
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const prisma = new PrismaClient()
 
-app.use(json());
-app.use(cors());
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
 
-app.get("/health", (req: Request, res: Response) => res.send("Hello world"));
-
-// app.use("/api");
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
