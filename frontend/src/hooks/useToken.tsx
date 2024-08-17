ee6c374eae1ebe1a7fd4f2ac53766bf482174de6
franciscojrdev <franciscojunior7222@gmail.com>
2024-08-17 18:13:44 -0300

    Login and Signup done

--- frontend/src/hooks/useToken.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import UserContext from "../contexts/AuthContext";
-
-export default function useToken() {
-  const { user } = useContext(UserContext);
-
-  return user.token;
-}
