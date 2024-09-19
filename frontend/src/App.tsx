6e5559c4ab27d3fda2e582989ef95937ba983972
Willreinaldo <willian.reinaldo.m@gmail.com>
2024-09-19 18:54:45 -0300

    fix rota

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,19 +9,12 @@ import SignInPage from "./pages/login/SignIn";
 import Agendamentos from "./pages/agendamento/Agendamentos";
 import GlobalStyle from "./Reset";
 import Layout from "./components/layout/Layout";
-<<<<<<<<< Temporary merge branch 1
-import Agendar from "./pages/agendar/agendar";
+import AgendarCortePage from "./pages/agendar/AgendarCortePage";
 import { NavbarProvider } from "./contexts/NavbarContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import Landpage from "./pages/landpage/LandPage";
 import VerifyCredentialToken from "./hooks/verifyCredential";
 
-=========
-import AgendarCortePage from "./pages/agendar/AgendarCortePage";
-import { NavbarProvider } from "./contexts/NavbarContext";  
-import { AuthProvider } from "./contexts/AuthContext";
- 
->>>>>>>>> Temporary merge branch 2
 const App: React.FC = () => {
   return (
     <Router>
@@ -41,12 +34,7 @@ const App: React.FC = () => {
             >
               <Route path="/local" element={<Local />} />
               <Route path="/profile" element={<Profile />} />
-              <Route path="/agendamentos" element={<Agendamentos />} />
-<<<<<<<<< Temporary merge branch 1
-              <Route path="/agendar" element={<Agendar />} />
-            </Route>
-            {/* Adicione outras rotas conforme necessário */}
-=========
+              <Route path="/agendamentos" element={<Agendamentos />} />   
               <Route path="/agendar" element={<AgendarCortePage/>} />
             </Route>
             <Route path="/login" element={<LoginPage />} />
